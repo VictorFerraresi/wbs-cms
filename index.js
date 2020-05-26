cfd18e8d16eaafb598d020a152ef03c8d40deab9
Vitor Vasconcellos <vvasconcellos1@gmail.com>
2020-05-26 20:41:21 -0300

    changed path for images and added ssl certificate

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const https = require('https');
 const app = express();
 const fs = require('fs');
 
 const port = 3007;
 
-app.use(function(req, res, next) {
+const privateKey = fs.readFileSync('./ssl/private.key');
+const certificate = fs.readFileSync('./ssl/certificate.crt');
+
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -15,13 +19,13 @@ app.use('/static', express.static('images'));
 app.get('/pages/:page', function (req, res) {
   const filePath = `./pages/${req.params.page}.json`;
   if (!fs.existsSync(filePath)) {
-    return res.status(400).send({"message": "Esta página não existe no CMS."});
+    return res.status(400).send({ "message": "Esta página não existe no CMS." });
   }
   const rawdata = fs.readFileSync(filePath);
   return res.send(JSON.parse(rawdata));
 });
 
-
-app.listen(port, function () {
-  console.log(`CMS listening on port ${port}!`);
-});
+https.createServer({
+  key: privateKey,
+  cert: certificate
+}, app).listen(port);
