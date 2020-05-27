f5b58177e93af64667aaafde763c31eb7cf038a4
Vitor Vasconcellos <vvasconcellos1@gmail.com>
2020-05-26 22:58:01 -0300

    updated gitignore and added cross-env to produciton and development mode

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const fs = require('fs');
 
 const port = 3007;
 
-const privateKey = fs.readFileSync('./ssl/private.key');
-const certificate = fs.readFileSync('./ssl/certificate.crt');
-
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -25,9 +22,22 @@ app.get('/pages/:page', function (req, res) {
     return res.send(JSON.parse(rawdata));
 });
 
-https.createServer({
-    key: privateKey,
-    cert: certificate
-}, app).listen(port, function() {
-    console.log(`CMS listening on port ${port}!`);
-});
+if (process.env.CMS_APP_STAGE === 'dev') {
+    app.listen(port, function () {
+        console.log(`[DEV MODE] CMS listening on port ${port}!`);
+    });
+} else {
+    const privateKey = fs.readFileSync('./ssl/private.key');
+    const certificate = fs.readFileSync('./ssl/certificate.crt');
+
+    https.createServer({
+        key: privateKey,
+        cert: certificate
+    }, app).listen(port, function () {
+        console.log(`CMS listening on port ${port}!`);
+    });
+}
+
+
+
+
