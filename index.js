feb668060ba86a84e51159a965da7f9b85747a13
Vitor Vasconcellos <vvasconcellos1@gmail.com>
2020-05-26 22:46:22 -0300

    added console.log to index and ssl to gitignore

--- index.js
+++ index.js
@@ -9,23 +9,25 @@ const privateKey = fs.readFileSync('./ssl/private.key');
 const certificate = fs.readFileSync('./ssl/certificate.crt');
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 app.use('/static', express.static('images'));
 
 app.get('/pages/:page', function (req, res) {
-  const filePath = `./pages/${req.params.page}.json`;
-  if (!fs.existsSync(filePath)) {
-    return res.status(400).send({ "message": "Esta página não existe no CMS." });
-  }
-  const rawdata = fs.readFileSync(filePath);
-  return res.send(JSON.parse(rawdata));
+    const filePath = `./pages/${req.params.page}.json`;
+    if (!fs.existsSync(filePath)) {
+        return res.status(400).send({ "message": "Esta página não existe no CMS." });
+    }
+    const rawdata = fs.readFileSync(filePath);
+    return res.send(JSON.parse(rawdata));
 });
 
 https.createServer({
-  key: privateKey,
-  cert: certificate
-}, app).listen(port);
+    key: privateKey,
+    cert: certificate
+}, app).listen(port, function() {
+    console.log(`CMS listening on port ${port}!`);
+});
